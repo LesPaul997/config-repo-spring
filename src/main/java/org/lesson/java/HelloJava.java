9855e96e1f902f7c5f2092909c540e3bc4fff130
Mario Giordano <164010608+LesPaul997@users.noreply.github.com>
2024-06-02 11:21:30 +0200

    added two java example clesses

---
+++ src/main/java/org/lesson/java/HelloJava.java
@@ -0,0 +1,9 @@
+package org.lesson.java; //Questo è il package che utilizzerò per questa classe
+
+public class HelloJava {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("Hello Spring");
+	}
+}
